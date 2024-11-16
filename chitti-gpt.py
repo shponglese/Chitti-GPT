8b782641e29d5ad09be85674655ae6fed469e76f
shponglese <saionthemail@gmail.com>
2024-11-15 16:24:12 -0800

    changes to file

--- chitti-gpt.py
+++ chitti-gpt.py
@@ -100,7 +100,28 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-    
+    def forward(self, idx):
+        #idx is of shape (B,T)
+        B, T = idx.size()
+        assert T <= self.config.black_size, f"Cannot foward sequence of length {T}, block size"
+        #forward the token and position embeddings
+        pos = torch.arange(0,T, dtype=torch.long, device=idx.device) #shape(T)
+        pos_emb = self.transformer.wpe(pos) #position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) #token embeddings of shape(B, T, n_embd)
+
+        x = tok_emb + pos_emb
+
+        #forward the blocks of the transformer
+
+        for block in self.transformer.h:
+            x.block(x)
+        
+        #forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+        return logits
+
+
     @classmethod
     def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from huggingface"""
@@ -128,12 +149,36 @@ class GPT(nn.Module):
 
         sd = model.state_dict()
         sd_keys = sd.keys()
-        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] #discord this mask/ buffer 
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] #discard this mask/ buffer, not a param
 
         #init a huggingface/transformers model
 
         model_hf = GPT2LMHeadModel.from_pretrained(model_type)
         sd_hf = model_hf.state_dict()
-
         
+        
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+
+model = GPT.from_pretrained('gpt2')
 
